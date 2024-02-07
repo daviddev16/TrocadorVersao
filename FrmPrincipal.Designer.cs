12f3b89e1c8c99687adb1cfb1853646ac205838d
David Duarte <37241920+daviddev16@users.noreply.github.com>
2024-02-06 22:03:13 -0300

    init

---
+++ FrmPrincipal.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+
+partial class FrmPrincipal
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.cmbBxSelecaoVersao = new System.Windows.Forms.ComboBox();
+            this.btnSelecionarVersao = new System.Windows.Forms.Button();
+            this.lsBxLog = new System.Windows.Forms.ListBox();
+            this.cmbBxZShop = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmbBxSelecaoVersao
+            // 
+            this.cmbBxSelecaoVersao.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBxSelecaoVersao.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbBxSelecaoVersao.FormattingEnabled = true;
+            this.cmbBxSelecaoVersao.Location = new System.Drawing.Point(12, 23);
+            this.cmbBxSelecaoVersao.Name = "cmbBxSelecaoVersao";
+            this.cmbBxSelecaoVersao.Size = new System.Drawing.Size(75, 21);
+            this.cmbBxSelecaoVersao.TabIndex = 0;
+            // 
+            // btnSelecionarVersao
+            // 
+            this.btnSelecionarVersao.Location = new System.Drawing.Point(12, 50);
+            this.btnSelecionarVersao.Name = "btnSelecionarVersao";
+            this.btnSelecionarVersao.Size = new System.Drawing.Size(169, 29);
+            this.btnSelecionarVersao.TabIndex = 1;
+            this.btnSelecionarVersao.Text = "Alterar Versão";
+            this.btnSelecionarVersao.UseVisualStyleBackColor = true;
+            this.btnSelecionarVersao.Click += new System.EventHandler(this.btnSelecionarVersao_Click);
+            // 
+            // lsBxLog
+            // 
+            this.lsBxLog.FormattingEnabled = true;
+            this.lsBxLog.Location = new System.Drawing.Point(187, 23);
+            this.lsBxLog.Name = "lsBxLog";
+            this.lsBxLog.Size = new System.Drawing.Size(321, 56);
+            this.lsBxLog.TabIndex = 2;
+            // 
+            // cmbBxZShop
+            // 
+            this.cmbBxZShop.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBxZShop.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbBxZShop.FormattingEnabled = true;
+            this.cmbBxZShop.Items.AddRange(new object[] {
+            "wshop.exe",
+            "ishop.exe"});
+            this.cmbBxZShop.Location = new System.Drawing.Point(93, 23);
+            this.cmbBxZShop.Name = "cmbBxZShop";
+            this.cmbBxZShop.Size = new System.Drawing.Size(88, 21);
+            this.cmbBxZShop.TabIndex = 3;
+            this.cmbBxZShop.SelectedIndexChanged += new System.EventHandler(this.cmbBxZShop_SelectedIndexChanged);
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 102);
+            this.Controls.Add(this.cmbBxZShop);
+            this.Controls.Add(this.lsBxLog);
+            this.Controls.Add(this.btnSelecionarVersao);
+            this.Controls.Add(this.cmbBxSelecaoVersao);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmPrincipal";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ComboBox cmbBxSelecaoVersao;
+    private System.Windows.Forms.Button btnSelecionarVersao;
+    private System.Windows.Forms.ListBox lsBxLog;
+    private System.Windows.Forms.ComboBox cmbBxZShop;
+}
+
---
+++ FrmPrincipal.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+public partial class FrmPrincipal : Form
+{
+    public static readonly string ROOT_ALTERDATA = @"C:\Program Files (x86)\Alterdata";
+
+    public struct ExecutavelShop
+    {
+        public readonly static ExecutavelShop VAZIO = new ExecutavelShop();
+        
+        public string DiretorioRaiz;
+        public string Caminho;
+        public string Versao;
+        public bool Valido { get => !string.IsNullOrWhiteSpace(Versao); }
+    }
+    
+    private string nomeExecutavelBase;
+    private List<ExecutavelShop> executaveisShop;
+    private ExecutavelShop executavelShopAtual;
+
+    public FrmPrincipal()
+    {
+        nomeExecutavelBase = "wshop.exe";
+        ReiniciarEstadoAplicacao();
+        InitializeComponent();
+    }
+
+    private void FrmPrincipal_Load(object sender, EventArgs e)
+    {
+        IniciarInterface();
+        cmbBxZShop.SelectedIndex = 0;
+    }
+
+    private void ReiniciarEstadoAplicacao()
+    {
+        if (executaveisShop != null)
+            executaveisShop.Clear();
+     
+        executaveisShop = ObterMultiExecutaveisShop(ROOT_ALTERDATA);
+        executavelShopAtual = ObterVersaoAtualShop(ROOT_ALTERDATA, nomeExecutavelBase);
+    }
+
+    private void IniciarInterface()
+    {
+        if (executavelShopAtual.Equals(ExecutavelShop.VAZIO))
+        {
+            MessageBox.Show("Não foi possível identificar o executável atual da pasta 'Shop'.");
+            Application.Exit();
+            return;
+        }
+        IniciarSeletorDeVersao(executaveisShop);
+    }
+
+    private void IniciarSeletorDeVersao(List<ExecutavelShop> executaveisShop)
+    {
+        cmbBxSelecaoVersao.Items.Clear();
+        foreach (ExecutavelShop executavelShop in executaveisShop)
+        {
+            if (!executavelShopAtual.Versao.Equals(executavelShop.Versao))
+            {
+                cmbBxSelecaoVersao.Items.Add(executavelShop.Versao);
+            }
+        }
+        cmbBxSelecaoVersao.SelectedIndex = 0;
+    }
+
+    private List<ExecutavelShop> ObterMultiExecutaveisShop(string rootPath)
+    {
+        List<ExecutavelShop> executaveisShop = new List<ExecutavelShop>();
+        foreach (string shopDirPath in Directory.GetDirectories(rootPath, "Shop*", SearchOption.TopDirectoryOnly))
+        {
+            ExecutavelShop executavelShop = ObterExecutavelShop(shopDirPath, nomeExecutavelBase);
+            executaveisShop.Add(executavelShop);
+        }
+        return executaveisShop;
+    }
+
+    private ExecutavelShop ObterExecutavelShop(string shopPath, string nomeExecutavel)
+    {
+        foreach (string caminhoExecutavelShop in Directory.GetFiles(shopPath, nomeExecutavel, SearchOption.TopDirectoryOnly))
+        {
+            ExecutavelShop executavelShop = new ExecutavelShop();
+            FileVersionInfo fileVersion = FileVersionInfo.GetVersionInfo(caminhoExecutavelShop);
+            executavelShop.DiretorioRaiz = caminhoExecutavelShop;
+            executavelShop.Caminho = caminhoExecutavelShop;
+            executavelShop.Versao = fileVersion.FileVersion.Substring(0, 6).Replace(".", "");
+            return executavelShop;
+        }
+        return ExecutavelShop.VAZIO;
+    }
+
+    private ExecutavelShop ObterVersaoAtualShop(string rootPath, string nomeExecutavel)
+    {
+        string shopAtualPath = Path.Combine(rootPath, "Shop");
+
+        if (File.Exists(shopAtualPath))
+            return ExecutavelShop.VAZIO;
+
+        return ObterExecutavelShop(shopAtualPath, nomeExecutavel);
+    }
+
+    private ExecutavelShop ObterExecutavelSelecionado()
+    {
+        string versaoSelecionada = cmbBxSelecaoVersao.Text;
+        if (!string.IsNullOrWhiteSpace(versaoSelecionada))
+        {
+            foreach (ExecutavelShop executavelShop in executaveisShop)
+            {
+                if (executavelShop.Versao.Equals(versaoSelecionada))
+                {
+                    return executavelShop;
+                }
+            }
+        }
+        return ExecutavelShop.VAZIO;
+    }
+
+    private void btnSelecionarVersao_Click(object sender, EventArgs e)
+    {
+        lsBxLog.Items.Clear();
+
+        VerificarSeVersaoAtualEhValida();
+        ExecutavelShop executavelShopDestino = ObterExecutavelSelecionado();
+        
+        RenomearPastas();
+        Thread.Sleep(2000);
+        RenomearPastas2(executavelShopDestino);
+        Thread.Sleep(500);
+        
+        MessageBox.Show("Versão alterada.");
+        
+        ReiniciarEstadoAplicacao();
+        IniciarInterface();
+    }
+
+    private string CaminhoFormatadoRoot(string relativoPath)
+    {
+        return Path.Combine(ROOT_ALTERDATA, relativoPath);
+    }
+    private void VerificarSeVersaoAtualEhValida()
+    {
+        if (executavelShopAtual.Equals(ExecutavelShop.VAZIO) || !executavelShopAtual.Valido)
+        {
+            MessageBox.Show("Não foi possível identificar o executável atual da pasta 'Shop'.");
+            Application.Exit();
+        }
+    }
+
+    private void RenomearPastas() 
+    {
+
+        string bibliotecaOrigemPath = CaminhoFormatadoRoot("Biblioteca");
+        string shopOrigemPath = CaminhoFormatadoRoot("Shop");
+        string modulosOrigemPath = CaminhoFormatadoRoot("Modulos");
+
+        string bibliotecaDestinoPath = CaminhoFormatadoRoot("Biblioteca_" + executavelShopAtual.Versao);
+        string shopDestinoPath = CaminhoFormatadoRoot("Shop_" + executavelShopAtual.Versao);
+        string modulosDestinoPath = CaminhoFormatadoRoot("Modulos_" + executavelShopAtual.Versao);
+
+        lsBxLog.Items.Add(NomeBonito(bibliotecaOrigemPath) + " para " + NomeBonito(bibliotecaDestinoPath));
+        Directory.Move(bibliotecaOrigemPath, bibliotecaDestinoPath);
+
+        lsBxLog.Items.Add(NomeBonito(shopOrigemPath) + " para " + NomeBonito(shopDestinoPath));
+        Directory.Move(shopOrigemPath, shopDestinoPath);
+
+        lsBxLog.Items.Add(NomeBonito(bibliotecaOrigemPath) + " para " + NomeBonito(bibliotecaDestinoPath));
+        Directory.Move(modulosOrigemPath, modulosDestinoPath);
+
+        Console.WriteLine("  ");
+    }
+
+    private void RenomearPastas2(ExecutavelShop executavelShopDestino)
+    {
+        string bibliotecaOrigemPath = CaminhoFormatadoRoot("Biblioteca_" + executavelShopDestino.Versao);
+        string shopOrigemPath = CaminhoFormatadoRoot("Shop_" + executavelShopDestino.Versao);
+        string modulosOrigemPath = CaminhoFormatadoRoot("Modulos_" + executavelShopDestino.Versao);
+
+        string bibliotecaDestinoPath = CaminhoFormatadoRoot("Biblioteca");
+        string shopDestinoPath = CaminhoFormatadoRoot("Shop");
+        string modulosDestinoPath = CaminhoFormatadoRoot("Modulos");
+
+        lsBxLog.Items.Add(NomeBonito(bibliotecaOrigemPath) + " para " + NomeBonito(bibliotecaDestinoPath));
+        Directory.Move(bibliotecaOrigemPath, bibliotecaDestinoPath);
+
+        lsBxLog.Items.Add(NomeBonito(shopOrigemPath) + " para " + NomeBonito(shopDestinoPath));
+        Directory.Move(shopOrigemPath, shopDestinoPath);
+
+        lsBxLog.Items.Add(NomeBonito(modulosOrigemPath) + " para " + NomeBonito(modulosDestinoPath));
+        Directory.Move(modulosOrigemPath, modulosDestinoPath);
+
+        Console.WriteLine("  ");
+    }
+
+    private string NomeBonito(string path)
+    {
+        return "\"Alterdata\\" + Path.GetFileName(path) + "\"";
+    }
+
+    private void cmbBxZShop_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        nomeExecutavelBase = cmbBxZShop.SelectedItem.ToString().Trim();
+    }
+}
